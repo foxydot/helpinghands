76d1b999a99131b83ac4966dd0de8e792283d0aa
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2013-10-04 14:44:32 -0400

    chenge to archive event, add gfplaceholder plugin

--- wp-content/themes/mittens/archive-event.php
+++ wp-content/themes/mittens/archive-event.php
@@ -3,8 +3,9 @@ remove_action('loop_start','msdlab_fancy_title');
 remove_action( 'genesis_before_post_content', 'genesis_post_info' );
 remove_action( 'genesis_after_post_content', 'genesis_post_meta' );
 remove_action('genesis_post_content', 'genesis_do_post_content');
-add_action('genesis_post_content','msd_add_gravity_form');
+remove_action('genesis_loop', 'genesis_do_loop');
+add_action('genesis_after_loop','msd_add_gravity_form');
 function msd_add_gravity_form(){
-    do_shortcode('[gravityform id="1" ajax="true"]');
+    print do_shortcode('[gravityform id="1" ajax="true"]');
 }
 genesis();
